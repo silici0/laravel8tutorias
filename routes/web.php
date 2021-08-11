4186cea03765a614906d6cf7e22dea0e17020c57
Rafael Cunha <silici0@gmail.com>
2021-08-10 21:20:57 -0300

    ep02 - Rotas

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('contato', function() {
+    return 'Página de contato';
+});
+
+Route::get('sobre', function() {
+    return 'Sobre nós';
+});
